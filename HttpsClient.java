0b0b1fc0c692780759859e0beb52d32f6c7d09c7
SivaramTrialBlazer <48651490+SivaramTrialBlazer@users.noreply.github.com>
2019-04-19 22:33:48 +0530

    Add files via upload

--- HttpsClient.java
+++ HttpsClient.java
@@ -23,7 +23,7 @@ public class HttpsClient{
    public static void main(String[] args)
    {
 	   int Todayopen, Yesterdayclose;
-	   String symbol="CRUDEOILM19APRFUT";
+	   String symbol="CRUDEOILM19MAYFUT";
 	   
 	 HttpsClient ter=new HttpsClient();
 	    try {
